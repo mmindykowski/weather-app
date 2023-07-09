e02626388ba85be9b108e4edff800ecadd08d3fd
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-09 17:11:10 +0200

    kompletna funkcja do pobierania danych z api

--- src/Weather.js
+++ src/Weather.js
@@ -3,16 +3,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Weather = () => {
-  const [weatherData, setWeather] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
 
   const getWeatherData = () => {
     axios.get("https://danepubliczne.imgw.pl/api/data/synop").then((res) => {
-      console.log(res);
+      setWeatherData(res.data)
     });
   };
   useEffect(() => {
     getWeatherData();
   }, []);
+
+  console.log(weatherData);
   return <div className="weather">Pogoda</div>;
 };
 
