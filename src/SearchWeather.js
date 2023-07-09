2bff22aef176f9c80e3eb3a38439b37e9325e35d
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-09 18:00:27 +0200

    Input w pliku SearchWeather

--- src/SearchWeather.js
+++ src/SearchWeather.js
@@ -1,3 +1,9 @@
-const SearchWeather = (params) => {
-    
-}
+const SearchWeather = (props) => {
+    return (
+        <div className="searchWeather">
+            Szukaj: <input type="text"/>
+        </div>
+    )
+};
+
+export default SearchWeather;
