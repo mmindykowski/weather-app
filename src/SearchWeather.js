260c45131b6c6daa883547cbae2ccc1897d7a60c
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-09 18:49:19 +0200

    Clean code

--- src/SearchWeather.js
+++ src/SearchWeather.js
@@ -3,7 +3,11 @@ const SearchWeather = (props) => {
     <div className="searchWeather">
       <form>
         <label htmlFor="searchWeather">Szukaj: </label>
-        <input type="text" id="searchWeather" onChange={(e)=>props.filterWeather(e.target.value)} />
+        <input
+          type="text"
+          id="searchWeather"
+          onChange={(e) => props.filterWeather(e.target.value)}
+        />
       </form>
     </div>
   );
