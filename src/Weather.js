50ad15002fd6aaec1804dd899b49434275e2f287
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-09 16:53:57 +0200

    ustawianie stanu

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,9 @@
+import axios from "axios";
+
+import { useState } from "react";
+
 const Weather = () => {
+    const [weatherData, setWeather] = useState([])
     return (
         <div className="weather">
             Pogoda
